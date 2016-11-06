12a3fa9ca2171e028ba26a65182cbab4eb6b4192
ctac1608 <sheludchenko.stas@gmail.com>
2016-11-06 18:04:17 +0300

    Service

---
+++ Kursach3/Scripts/Services/CreativeService.js
@@ -0,0 +1,20 @@
+﻿app.service("creativeService", function ($http) {
+
+    this.createCreative = function (creative) {
+        var creative = {
+            Name: creative.Name,
+            Tags: creative.Tags,
+            Rank: 0,
+            Count: 0
+        }
+
+        $.post("/Creative/Create/", creative, function (response) {
+            creative.Id = response;
+            //notes.push(note);
+            //callback();
+        });
+    };
+
+
+
+});
